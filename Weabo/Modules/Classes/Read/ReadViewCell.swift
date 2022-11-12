f01834c950869a34e9de87034caf86a02bb2e69d
faris <farisariep@gmail.com>
2022-11-12 19:55:00 +0700

    change present to push on readvc

---
+++ Weabo/Modules/Classes/Read/ReadViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReadViewCell.swift
+//  ComicKid
+//
+//  Created by yoga arie on 11/11/22.
+//
+
+import UIKit
+
+class ReadViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var imgView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
